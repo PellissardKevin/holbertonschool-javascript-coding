d553d90cf9e2a046cf05f7fff28a5758698be501
Kevin.P <kevin.pellissard@gmail.com>
2024-01-22 22:22:54 +0100

    update error messzge task 4

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -11,7 +11,10 @@ const url = 'https://swapi-api.hbtn.io/api/people/18';
 request(url, { json: true }, (error, response, body) => {
     if (response && response.statusCode === 200) {
       console.log(body.films.length);
-    } else {
+    } else if (error) {
       console.error('Error:', error);
-    }
+    } else {
+        console.error('code:', response.statusCode);
+        return;
+      }
 });
