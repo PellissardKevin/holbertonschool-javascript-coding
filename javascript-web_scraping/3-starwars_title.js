a25fe699c1bfc9bdafce384395e275057560366d
Kevin.P <kevin.pellissard@gmail.com>
2024-01-23 14:55:58 +0100

    update task 3

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,13 +1,14 @@
 #!/usr/bin/node
 /* eslint-disable no-undef */
 
+
 const request = require('request');
 const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
 
 request.get(url, (error, response, body) => {
   if (error) {
-    console.error(`Error: ${error}`);
+    console.error(`${error}`);
   } else {
-    console.log(JSON.parse(body).title);
+    console.log(`${JSON.parse(body).title}`);
   }
 });
