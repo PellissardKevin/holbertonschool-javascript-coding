ecb7598f467082470984859cd77c926906f71605
Kevin.P <kevin.pellissard@gmail.com>
2024-01-31 20:54:00 +0100

    update task 1 for detecting ctrl + c

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,15 +1,26 @@
 /* eslint-disable no-undef */
-function welcome() {
-  console.log('Welcome to Holberton School, what is your name?\n');
-  process.stdin.on('data', (data) => {
-    console.log(`Your name is: ${data.toString()}`);
-    console.log('This important software is now closing\n');
-    process.exit();
-  });
+const readline = require("readline")
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
+
+function ask() {
+  rl.question('Welcome to Holberton School, what is your name?\n', (answer) => {
+    process.stdin.setRawMode(true);
+    process.stdin.on("keypress", function(chunk, key) {
+      if(key && key.name === "c" && key.ctrl) {
+        console.log('This important software is now closing');
+        process.exit();
+      }
+    });
+    rl.write(`Your name is: ${answer}\n`)
+  })
 }
 
-module.exports = welcome;
+module.exports = ask;
 
 if (require.main === module) {
-  welcome();
+  ask();
 }
