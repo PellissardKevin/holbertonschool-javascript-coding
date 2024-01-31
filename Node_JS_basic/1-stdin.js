13309b2a505faa2c1c536943677ca04a345612ee
Kevin.P <kevin.pellissard@gmail.com>
2024-01-31 20:55:37 +0100

    update something for task 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -8,15 +8,10 @@ const rl = readline.createInterface({
 
 function ask() {
   rl.question('Welcome to Holberton School, what is your name?\n', (answer) => {
-    process.stdin.setRawMode(true);
-    process.stdin.on("keypress", function(chunk, key) {
-      if(key && key.name === "c" && key.ctrl) {
-        console.log('This important software is now closing');
-        process.exit();
-      }
-    });
     rl.write(`Your name is: ${answer}\n`)
-  })
+    console.log('This important software is now closing');
+    process.exit();
+  });
 }
 
 module.exports = ask;
