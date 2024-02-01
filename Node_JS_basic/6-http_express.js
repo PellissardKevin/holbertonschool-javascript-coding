f7d35c62c537b1a76568f5ab0b0497d229f4d701
Kevin.P <kevin.pellissard@gmail.com>
2024-02-01 08:34:33 +0100

    Remove if in task 6 and add app.listen

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -1,16 +1,10 @@
 const express = require('express');
 
 const app = express();
-const port = 1245;
 
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(1245);
 module.exports = app;
-
-if (require.main === module) {
-  app.get('/', (request, response) => {
-    response.send('Hello Holberton School!');
-  });
-
-  app.listen(port, () => {
-    console.log(`Server is running : http://localhost:${port}/`);
-  });
-}
