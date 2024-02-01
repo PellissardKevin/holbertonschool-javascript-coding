3d24aa02c29ac3654290403352800c1939b54b5b
Kevin.P <kevin.pellissard@gmail.com>
2024-02-01 08:26:05 +0100

    Back in previous version of task 1, update console.log by stdout.write

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,24 +1,15 @@
 /* eslint-disable no-undef */
-console.log('Welcome to Holberton School, what is your name?');
-
-const readline = require('readline');
-
-const inputRead = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-inputRead.on('line', (name) => {
-  console.log(`Your name is: ${name}`);
-  process.stdin.on("keypress", function (chunk, key) {
-    process.stdin.setRawMode(true);
-    if (key && key.name === "c" && key.ctrl) {
-      console.log('This important software is now closing');
-      inputRead.close();
-    }
+function welcome() {
+  process.stdout.write('Welcome to Holberton School, what is your name?\n');
+  process.stdin.on('data', (input) => {
+    process.stdout.write(`Your name is: ${input.toString()}`);
+    process.stdout.write('This important software is now closing\n');
+    process.exit();
   });
+}
 
-});
+module.exports = welcome;
 
-module.exports = inputRead;
-
+if (require.main === module) {
+  welcome();
+}
